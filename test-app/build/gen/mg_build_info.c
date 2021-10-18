bff0a5da9a9b02b554acf9a9cddf8e7a89357925
Syed Furquan Ahmad <furquan@pm.me>
2021-10-18 23:06:26 +0530

    Remove test-app

--- test-app/build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20211018-164313/2.19.1-gd52032f";
-const char *mg_build_timestamp = "2021-10-18T16:43:13Z";
-const char *mg_build_version = "2.19.1";
