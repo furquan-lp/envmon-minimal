4d67fd0eb55a6c58fb18b564628a8470052feb29
Syed Furquan Ahmad <furquan@pm.me>
2021-10-18 23:45:09 +0530

    Removed extra files from build/

--- build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20211018-181150";
-const char *build_timestamp = "2021-10-18T18:11:50Z";
-const char *build_version = "0.1";
--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20211018-181148/2.19.1-gd52032f";
-const char *mg_build_timestamp = "2021-10-18T18:11:48Z";
-const char *mg_build_version = "2.19.1";
