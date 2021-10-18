cf2ea0ea95d36308ef5fa3cf7795ba3f47b10c35
Syed Furquan Ahmad <furquan@pm.me>
2021-10-18 23:39:25 +0530

    Added function for humidity

--- fs/init.js
+++ fs/init.js
@@ -3,15 +3,16 @@ load('api_rpc.js');
 load('api_dht.js');
 load('api_timer.js');
 
-print('Begin...\n')
+print('-Environment monitor begin-')
 
 let pin = Cfg.get('app.pin');
 let dht = DHT.create(pin, DHT.DHT22);
 
 Timer.set(1000, true, function() {
   print('Temperature:', dht.getTemp());
+  print('Humidity: ', dht.getHumidity());
 }, null);
 
-RPC.addHandler('Temp.Read', function(args) {
+/*RPC.addHandler('Temp.Read', function(args) {
   return { value: dht.getTemp() };
-});
+});*/
