74888bb1e51ed0771c1ed13767485f24494524a3
Syed Furquan Ahmad <furquan@pm.me>
2021-10-21 23:57:52 +0530

    Added envmon.h

---
+++ src/envmon/envmon.h
@@ -0,0 +1,23 @@
+const char* ssid = "****"
+const char* password = "****";
+
+String interstitial[] = { "<!DOCTYPE html>\
+<html>\
+<title>ESP32 Environment Monitor</title>\
+<style type=\"text/css\">\
+body{\
+font-family: Sans-Serif;\color:#444;\
+}\
+</style>\
+<body>\
+<h1>ESP32 Environment Monitor v0.x</h1>\
+<h2>Temperature: ", "*C Humidity: ", "</h2><script>\
+setTimeout(function(){\
+window.location.reload(1);\
+}, 3000);\
+</script>\
+</body>\
+</html>"
+};
+
+const int interstitial_length = 3;
