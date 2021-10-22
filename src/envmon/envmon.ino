0505b7f410d97da876d4c7ad0bcc0d06aab6d9ae
Syed Furquan Ahmad <furquan@pm.me>
2021-10-22 21:22:05 +0530

    Fixed missing DHT init, semicolon; modified to use spaces instead of tabs

--- src/envmon/envmon.ino
+++ src/envmon/envmon.ino
@@ -9,45 +9,46 @@ WebServer server(80);
 String HTML = String(interstitial[0]);
 
 void setup() {
-	Serial.begin(115200);
-	Serial.println("Connecting to ");
-	Serial.println(ssid);
+  init_DHT();
+    Serial.begin(115200);
+    Serial.println("Connecting to ");
+    Serial.println(ssid);
 
-	WiFi.begin(ssid, password);
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(1000);
-		Serial.print(".");
-	}
-	Serial.println("\nWiFi connected successfully");
-	Serial.print("Got IP: ");
-	Serial.println(WiFi.localIP());
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("\nWiFi connected successfully");
+    Serial.print("Got IP: ");
+    Serial.println(WiFi.localIP());
 
-	handle_Strings();
-	delay(100);
-	server.on("/", handle_root);
-	server.begin();
-	Serial.println("HTTP server started");
-	delay(100); 
+    handle_Strings();
+    delay(100);
+    server.on("/", handle_root);
+    server.begin();
+    Serial.println("HTTP server started");
+    delay(100); 
 }
 
 void loop() {
-	handle_Strings();
-	delay(100);
-	server.handleClient();
-	delay(1000);
+    handle_Strings();
+    delay(100);
+    server.handleClient();
+    delay(1000);
 }
 
 // Handle root url (/)
 void handle_root() {
-	server.send(200, "text/html", HTML);
+    server.send(200, "text/html", HTML);
 }
 
 // ** Also handles sensor data
 void handle_Strings() {
-	String str1 = interstitial[0];
-	str1.concat(get_temp());
-	str1.concat(interstitial[1]);
-	str1.concat(get_humid());
-	str1.concat(interstitial[2]);
-	HTML = String(str1);
+    String str1 = interstitial[0];
+    str1.concat(get_temp());
+    str1.concat(interstitial[1]);
+    str1.concat(get_humid());
+    str1.concat(interstitial[2]);
+    HTML = String(str1);
 }
