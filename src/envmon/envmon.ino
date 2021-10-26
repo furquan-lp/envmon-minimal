01073aa6db09d79ccedced0b20a8373145c9a7a6
Syed Furquan Ahmad <furquan@pm.me>
2021-10-26 22:17:56 +0530

    Removed String manipulation with snprintf() and format directives; this leads to a removal of code related to the Strings and functions since everything is handled within handle_root()

--- src/envmon/envmon.ino
+++ src/envmon/envmon.ino
@@ -5,26 +5,22 @@
 
 WebServer server(80);
 
-String HTML = "";
-String sensors_data[] = { "unavailable", "unavailable" };
-
 void setup() {
     init_DHT();
     pinMode(ONBOARD_LED, OUTPUT);
+    
     Serial.begin(115200);
     Serial.println("Connecting to ");
     Serial.println(ssid);
-
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
         Serial.print(".");
     }
-    Serial.println("\nWiFi connected successfully");
-    Serial.print("Got IP: ");
+    Serial.println("\nWiFi connected");
+    Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 
-    update_html();
     delay(100);
     server.on("/", handle_root);
     server.begin();
@@ -34,29 +30,15 @@ void setup() {
 
 void loop() {
     digitalWrite(ONBOARD_LED, HIGH);
-    update_sensors_data();
-    update_html();
     delay(100);
-    digitalWrite(ONBOARD_LED, LOW);
     server.handleClient();
+    digitalWrite(ONBOARD_LED, LOW);
     delay(2800);
 }
 
-// Handle root url (/)
+// Update the sensors data in the HTML and handle the root url (/)
 void handle_root() {
-    server.send(200, "text/html", HTML);
-}
-
-void update_sensors_data() {
-    sensors_data[0] = String(get_temp());
-    sensors_data[1] = String(get_humid());
-}
-
-void update_html() {
-    HTML = String(html_data);
-    for (uint8_t i = 0; i < 2; i++) {
-        String datstr = String(DATA_STR);
-        datstr.concat(i);
-        HTML.replace(datstr, sensors_data[i]);
-    }
+    char html[strlen(html_data) + 16];
+    snprintf(html, sizeof(html), html_data, get_temp(), get_humid());
+    server.send(200, "text/html", html);
 }
