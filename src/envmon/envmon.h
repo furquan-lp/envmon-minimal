29dc72170c8364b2855c86abbdea8915fa1335f4
Syed Furquan Ahmad <furquan@pm.me>
2021-10-27 23:46:55 +0530

    Added hourly uptime, LED_PIN is 13

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -1,7 +1,7 @@
 #ifndef ENVMON_H
 #define ENVMON_H
 
-#define ONBOARD_LED 2
+#define LED_PIN 13
 
 const char* ssid = "****";
 const char* password = "****";
@@ -20,8 +20,8 @@ background-color: #FFFFCC\
 <div class=\"esp32data\">\
 <h1>ESP32 Environment Monitor v0.x</h1>\
 <h2>Temperature: %.2f&deg;C Humidity: %.2f&#37;</h2>\
-<h4>Up since %d:%d</h4></div>\
-<p style=\"font-family:serif\">Last updated: 2021-10-26 Copyright (C) 2021 Syed Furquan Ahmad</p>\
+<h4>Up since %02d:%02d:%02d</h4></div>\
+<p style=\"font-family:serif\">Last updated: 2021-10-27 Copyright (C) 2021 Syed Furquan Ahmad</p>\
 <script>\
 setTimeout(function(){\
 window.location.reload(1);\
