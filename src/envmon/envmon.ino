fe8da1c3d1c6b3587389cd751331601088f474b2
Syed Furquan Ahmad <furquan@pm.me>
2021-11-02 00:51:09 +0530

    Messages now display on the connected I2C character LCD regarding the WiFi connection

--- src/envmon/envmon.ino
+++ src/envmon/envmon.ino
@@ -7,25 +7,36 @@
 void setup() {
     init_DHT();
     pinMode(LED_PIN, OUTPUT);
+    clcd.init();
     
     Serial.begin(115200);
     Serial.println("Connecting to ");
     Serial.println(ssid);
+    clcd.printLCD("Connecting WiFi");
     WiFi.begin(ssid, password);
+    clcd.selectLine(1);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
         Serial.print(".");
+        clcd.printLCD(".");
     }
+    clcd.clearLCD();
     Serial.println("\nWiFi connected");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
+    clcd.printLCD("IP");
+    clcd.printLCD(WiFi.localIP().toString());
 
     if (MDNS.begin(mdns_url)) {
         Serial.print("MDNS responder started at: http://");
         Serial.print(mdns_url);
         Serial.println(".local/");
+        char url[16];
+        sprintf(url, "%s.local/", mdns_url);
+        clcd.printAt(url, 1);
     } else {
         Serial.println("Error setting up MDNS responder!");
+        clcd.printAt("MDNS.begin fail.", 1);
     }
 
     delay(100);
@@ -39,6 +50,14 @@ void setup() {
 void loop() {
     delay(100);
     server.handleClient();
+}
+
+void printStatusLCD() {
+    clcd.printLCD("IP");
+    clcd.printLCD(WiFi.localIP().toString());
+    char url[16];
+    sprintf(url, "%s.local/", mdns_url);
+    clcd.printAt(url, 1);
 }
 
 void handle_root() {
