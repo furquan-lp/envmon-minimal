58fdc7b71493433417e6c629c8f7266402c92831
Syed Furquan Ahmad <furquan@pm.me>
2021-11-02 01:37:11 +0530

    Edited version to 1.1 (for new release)

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -21,7 +21,7 @@ background-color: #FFFFCC\
 </style>\
 <body>\
 <div class=\"esp32data\">\
-<h1>ESP32 Environment Monitor v0.x</h1>\
+<h1>ESP32 Environment Monitor v1.1</h1>\
 <h2>Temperature: %.2f&deg;C Humidity: %.2f&#37;</h2>\
 <h4>Up since %02d:%02d:%02d</h4></div>\
 <p style=\"font-family:serif\">Last updated: 2021-10-27 Copyright (C) 2021 Syed Furquan Ahmad</p>\
