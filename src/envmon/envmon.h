42fa27e605458069858cc61964c234f1ef821436
Syed Furquan Ahmad <furquan@pm.me>
2021-11-02 19:24:15 +0530

    SPIFFS: html_data is now read from index.html, css_data is added to hold style.css, added read_html_css() function

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -8,34 +8,12 @@
 const char* ssid = "****";
 const char* password = "****";
 const char* mdns_url = "esp32env";
-const char* html_data = "<!DOCTYPE html>\
-<html>\
-<title>ESP32 Environment Monitor</title>\
-<style type=\"text/css\">\
-body{\
-font-family: Sans-Serif;color:#444;\
-}\
-.esp32data {\
-background-color: #FFFFCC\
-}\
-</style>\
-<body>\
-<div class=\"esp32data\">\
-<h1>ESP32 Environment Monitor v1.1</h1>\
-<h2>Temperature: %.2f&deg;C Humidity: %.2f&#37;</h2>\
-<h4>Up since %02d:%02d:%02d</h4></div>\
-<p style=\"font-family:serif\">Last updated: 2021-11-02 Copyright (C) 2021 Syed Furquan Ahmad</p>\
-<script>\
-setTimeout(function(){\
-window.location.reload(1);\
-}, 3000);\
-</script>\
-<br>\
-</body>\
-</html>";
+char html_data[1000];
+char css_data[500];
 
 WebServer server(HTTP_PORT);
 CharacterLCD clcd(0x27, 16, 2);
 
+void read_html_css(void);
 void handle_root(void);
 #endif
