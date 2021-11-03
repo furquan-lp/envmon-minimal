fda07e13cbfa5d3106437800d74318e3f6bf2fa8
Syed Furquan Ahmad <furquan@pm.me>
2021-11-03 22:32:50 +0530

    Use manifest constants for HTML/CSS data buffer sizes

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -4,12 +4,14 @@
 
 #define LED_PIN 13
 #define HTTP_PORT 80
+#define HTML_DATA_BUF 1000
+#define CSS_DATA_BUF 500
 
 const char* ssid = "****";
 const char* password = "****";
 const char* mdns_url = "esp32env";
-char html_data[1000];
-char css_data[500];
+char html_data[HTML_DATA_BUF];
+char css_data[CSS_DATA_BUF];
 
 WebServer server(HTTP_PORT);
 CharacterLCD clcd(0x27, 16, 2);
