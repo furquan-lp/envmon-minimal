e354549588f6d878a9797c6b17c1ad92bc133406
Syed Furquan Ahmad <furquan@pm.me>
2021-12-26 10:38:04 +0530

    Removed CLCD header file and functions

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -1,6 +1,5 @@
 #ifndef ENVMON_H
 #define ENVMON_H
-#include "CLCD_I2C.h"
 
 #define LED_PIN 13
 #define HTTP_PORT 80
@@ -14,9 +13,7 @@ char html_data[HTML_DATA_BUF];
 char css_data[CSS_DATA_BUF];
 
 WebServer server(HTTP_PORT);
-CharacterLCD clcd(0x27, 16, 2);
 
 void read_html_css(void);
-void show_err_lcd(const char* line1, const char* line2);
 void handle_root(void);
 #endif
