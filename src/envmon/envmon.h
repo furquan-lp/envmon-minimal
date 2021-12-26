f168830fa23f8fd48b56ef8d72540feb044e8b53
Syed Furquan Ahmad <furquan@pm.me>
2021-12-26 10:53:37 +0530

    Removed the HTML/CSS buffers, replaced with a simple format string for holding the raw sensor data

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -3,14 +3,11 @@
 
 #define LED_PIN 13
 #define HTTP_PORT 80
-#define HTML_DATA_BUF 1000
-#define CSS_DATA_BUF 500
 
 const char* ssid = "****";
 const char* password = "****";
 const char* mdns_url = "esp32env";
-char html_data[HTML_DATA_BUF];
-char css_data[CSS_DATA_BUF];
+const char* data_str = "%.2f %.2f %02d %02d %02d";
 
 WebServer server(HTTP_PORT);
 
