95121f5b04ec36cd8e4acf26cea5eef72f365f0a
Syed Furquan Ahmad <furquan@pm.me>
2022-03-27 20:07:30 +0530

    Output data formatted into JSON template instead of a plain-text template

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -7,7 +7,7 @@
 const char* ssid = "****";
 const char* password = "****";
 const char* mdns_url = "esp32env";
-const char* data_str = "%.2f %.2f %02d %02d %02d";
+const char* data_str = "[{\"backend\":\"envmon-minimal\",\"version\":\"1.3J\",\"temp\":\"%.2f\",\"humidity\":\"%.2f\",\"hours\":\"%02d\",\"minutes\":\"%02d\",\"seconds\":\"%02d\"}]";
 
 WebServer server(HTTP_PORT);
 
