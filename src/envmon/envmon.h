df25390a9761986caa630d5c02179ec5314801e3
Syed F Ahmad <furquan@protonmail.com>
2023-03-17 00:28:36 +0530

    Changed version to 1.4A-20231703

--- src/envmon/envmon.h
+++ src/envmon/envmon.h
@@ -8,8 +8,8 @@ const char* ssid = "****";
 const char* password = "****";
 const char* mdns_url = "envmon";
 const char* data_str =
-    "[{\"backend\":{\"name\":\"envmon-minimal\",\"version\":\"1.32J\"},"
-    "\"envdata\":{\"temp\":\"%.2f\",\"humidity\":\"%.2f\",\"co2\":\"%d\"},"
+    "[{\"backend\":{\"name\":\"envmon-minimal\",\"version\":\"1.4A-20231703\"}"
+    ",\"envdata\":{\"temp\":\"%.2f\",\"humidity\":\"%.2f\",\"co2\":\"%d\"},"
     "\"uptime\":{\"hours\":\"%02d\",\"minutes\":\"%02d\",\"seconds\":\"%02d\"}}"
     "]";
 
